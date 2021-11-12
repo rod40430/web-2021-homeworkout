bb2715631a21490202ad9ce8b7ffcc5e013702ac
rod40430 <xogns40430@naver.com>
2021-11-12 16:02:49 +0900

    logout수정

--- php/logout.php
+++ php/logout.php
@@ -4,5 +4,5 @@ session_destroy();
 ?>
 <script>
     alert("로그아웃되었습니다.");
-    location.href = "http://192.168.20.142/web/login/index.php";
+    location.href = "http://192.168.20.142/web/login.php";
 </script>
