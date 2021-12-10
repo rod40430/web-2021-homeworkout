d25172c9dc6fb461d6ba7a3282cee01e72c1dd78
rod40430 <xogns40430@naver.com>
2021-12-10 14:09:38 +0900

    야발

--- resister.php
+++ resister.php
@@ -29,11 +29,11 @@
             <div id="jcon">
                 <div class="join_text">회원가입</div>
                 <form method="post" action="./php/signup.php">
-                    <div class="u_infor"><div class="j_text"></div><input class="j_id" type="email" name="j_id" placeholder="이메일 주소"></div><br>
-                    <div class="u_infor"><div class="j_text"></div><input class="j_pass" type="password" name="j_password" placeholder="비밀번호"></div><br>
-                    <div class="u_infor"><div class="j_text"></div><input class="j_pass_ck" type="password" name="j_pass_ck" placeholder="비밀번호 확인"></div><br>
-                    <div class="u_infor"><div class="j_text"></div><input class="j_name" type="text" name="j_name"placeholder="이름"></div><br>
-                    <div class="u_infor"><div class="j_text"></div><input class="j_phone" type="text" name="j_phone" placeholder="휴대폰번호(번호만입력)"></div><br>
+                    <div class="u_infor"><input class="j_id" type="email" name="j_id" placeholder="이메일 주소"></div><br>
+                    <div class="u_infor"><input class="j_pass" type="password" name="j_password" placeholder="비밀번호"></div><br>
+                    <div class="u_infor"><input class="j_pass_ck" type="password" name="j_pass_ck" placeholder="비밀번호 확인"></div><br>
+                    <div class="u_infor"><input class="j_name" type="text" name="j_name"placeholder="이름"></div><br>
+                    <div class="u_infor"><input class="j_phone" type="text" name="j_phone" placeholder="휴대폰번호(번호만입력)"></div><br>
                     <input type="submit" value="회원가입" id="user_button"><br>
                 </form>
                 <a href="./login.php"><button id="exit_btn">이미 회원이신가요?&nbsp;<p>로그인하기</p></button></a>
